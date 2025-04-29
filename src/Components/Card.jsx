c1620f94f85fbb0afad606b790511c1b79a2b7e7
sajal <Jainsajal519@gmail.com>
2025-04-29 16:14:29 +0530

    Final check

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Card() {
   return (
     <div className='w-full h-screen bg-zinc-200'>
-      <div className='px-3 py-4 bg-zinc-100 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-lg shadow-lg'>
+      <div className='px-3 py-4 bg-zinc-100 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md'>
          <h1 className='font-semibold '>Card</h1>
         </div>
     </div>
