32c4b9b31cf507ca8f8453bab6f923a56fc4e98a
JAIN Sajal <sajal.jain@soprasteria.com>
2025-04-29 16:17:30 +0530

    Reverse Push confirm

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -4,7 +4,7 @@ function Card() {
   return (
     <div className='w-full h-screen bg-zinc-200'>
       <div className='px-3 py-4 bg-zinc-100 absolute left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 rounded-md'>
-         <h1 className='font-semibold '>Card</h1>
+         <h1 className='font-semibold '>Cards</h1>
         </div>
     </div>
   )
