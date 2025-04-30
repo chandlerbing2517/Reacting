21c73a96790721ff7b7d68dc0085cbcf08dc27ca
sajal <Jainsajal519@gmail.com>
2025-04-30 23:18:43 +0530

    Use State

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import React from 'react'
 import Card from './Components/Card'
-const data=["Sajal", "Videh", "Sagar"];
+
 function App() {
+  const [score, setScore] = React.useState({name: "sajal", isAllowed: false});
+
   return (
-    <div>
-        <Card></Card>
-    </div>
+   <div>
+   <h1 className='p-4'>{score.name}</h1>
+   <h1 className='p-4'>{score.isAllowed.toString()}</h1>
+   <button onClick={()=>{
+    setScore({...score, isAllowed: !score.isAllowed})
+  }} className={`px-2 py-1 rounded mt-2 ${score.isAllowed ? 'bg-red-300' : 'bg-blue-300'} text-xs text-zinc-200`}>Click me</button>
+   </div>
+        
+
   );
 }
 
