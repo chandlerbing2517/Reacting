970f5fd6b52df85a2798b838fd35b4a069a13392
JAIN Sajal <sajal.jain@soprasteria.com>
2025-05-01 22:55:59 +0530

    Final usestate

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,21 @@ import React from 'react'
 import Card from './Components/Card'
 
 function App() {
-  const [arr, setarr] = React.useState([1,2,3,4,5,6,7,8,9,10]);
+  const [arr, setarr] = React.useState([{name:"John", age: 20}, {name:"Doe", age: 25}, {name:"Jane", age: 30}]);
 
   return (
    <div>
-    {arr.map(item=><h1>
-          {item}
+    {arr.map(item=> 
+    <div>
+      <h1>
+          {item.name}
         </h1>
+        <h1>
+          {item.age}
+        </h1>
+    </div>
     )}
-   <button onClick={()=>setarr(()=>arr.filter((item,index)=> index!=arr.length-1))} className='px-2 py-1 rounded mt-2 bg-red-300 text-xs text-zinc-200'>Click me</button>
+   <button onClick={()=>setarr(()=> arr.map(item=> item.name === "John" ?  {name:"John", age: item.age+1} : item ))} className='px-2 py-1 rounded mt-2 bg-red-300 text-xs text-zinc-200'>Click me</button>
    </div>
         
 
