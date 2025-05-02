440336e719ba29070410264dc02661a592a03283
JAIN Sajal <sajal.jain@soprasteria.com>
2025-05-02 17:49:08 +0530

    Props on big data

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,11 +1,19 @@
 import React from 'react';
 
-function Card({text,color}) {
+function Card({name, image, profession}) {
 
   return (
-        <button className={`px-2 py-2 ${color} text-black rounded-md m-3`}>
-          {text}
-          </button>      
+
+      <div className='w-52 bg-white rounded-md overflow-hidden'>
+        <div className='w-full h-32 bg-sky-200'>
+          <img src={image} className='w-full h-full object-cover'></img>
+        </div>
+          <div className='w-full p-3'>
+          <h3 className='text-sm'>{name}</h3>
+          <h5 className='text-xs'>{profession}</h5>
+          <button className='px-3 py-1 bg-blue-500 text-xs text-white font-semibold rounded-md mt-1'> Add Friend</button>
+        </div>
+      </div>
   );
 }
 
