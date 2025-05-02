4589ef8449bf8b5da55b59db3c46dd935aed4b5a
sajal <Jainsajal519@gmail.com>
2025-05-02 12:30:11 +0530

    Use State demo

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,9 @@ import React from 'react'
 import Card from './Components/Card'
 
 function App() {
-  const [arr, setarr] = React.useState([{name:"John", age: 20}, {name:"Doe", age: 25}, {name:"Jane", age: 30}]);
-
   return (
    <div>
-    {arr.map(item=> 
-    <div>
-      <h1>
-          {item.name}
-        </h1>
-        <h1>
-          {item.age}
-        </h1>
-    </div>
-    )}
-   <button onClick={()=>setarr(()=> arr.map(item=> item.name === "John" ?  {name:"John", age: item.age+1} : item ))} className='px-2 py-1 rounded mt-2 bg-red-300 text-xs text-zinc-200'>Click me</button>
+    <Card/>
    </div>
         
 
--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,24 +1,17 @@
 import React from 'react';
-
-
-function handlerover(){
-  alert("working!");
-}
-const data = [
-  {image: 'https://images.unsplash.com/photo-1633174524778-61a18ee54490?q=80&w=2096&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',name: "Amazon Basics", Description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quisquam, voluptatibus.", instock: true},
-  {image: 'https://media.istockphoto.com/id/1288818126/photo/toothbrushes-on-pink-background-with-copy-space-top-view.jpg?s=1024x1024&w=is&k=20&c=vcx9PTsPR7J-Cy6P4QRtU6zAV7ms0jMQS5xJVJX9V0Q=', name: "Daily Objects", Description: "Lorem ipsum dolor sit amet consectetur adipisicing", instock: false},
-  {image: 'https://images.unsplash.com/photo-1563203369-26f2e4a5ccf7?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',name: "Apple", Description: "Lorem ipsum dolor ", instock: true},
-]
+import { FaArrowRightLong } from "react-icons/fa6";
 function Card() {
+    const [val, setVal] = React.useState(false);
+
   return (
-      <div className='w-full h-screen bg-zinc-200 flex flex-col gap-10 items-center justify-center'>
-            {data.map((item,value)=>(
-                        <div className='w-90 px-3 py-2 bg-white rounded-md'>
-            <h3 className='font-semibold text-xl'> {item.name}</h3>
-            <p className='text-xs mt-2'> Lorem ipsum dolor sit amet.</p>
-            <button onMouseOver={handlerover} className='bg-blue-300 rounded mt-3 px-2 py-1 text-sm text-zinc-100'>Download Now!</button>
-          </div>
-            ))}
+      <div className='w-full h-screen bg-zinc-100 flex justify-center items-center'>
+        <div className=' relative w-72 h-52 bg-zinc-300 flex rounded-md overflow-hidden'>
+        <img className={` shrink-0 w-full h-full transition-transform duration-500 ease-in-out ${val ? "-translate-x-[0%]" : "-translate-x-[100%]"} object-cover`} src='https://images.unsplash.com/photo-1743309411498-a0f4f4b96b65?q=80&w=2940&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <img className={` shrink-0 w-full h-full transition-transform duration-500 ease-in-out ${val ? "-translate-x-[0%]" : "-translate-x-[100%]"} object-cover`} src='https://images.unsplash.com/photo-1745676353163-539761e42831?q=80&w=3087&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'></img>
+        <span onClick= {()=>setVal(()=>!val)} className='w-10 h-10 bg-[#dadada8b] absolute bottom-[0%] flex items-center justify-center left-1/2 rounded-full -translate-x-[50%] -translate-y-[50%]'>
+          <FaArrowRightLong size={"0.7em"}/>
+        </span>
+        </div>
       </div>
   );
 }
