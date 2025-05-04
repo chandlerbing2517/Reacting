38aa3abbff6d87d0adb12649be36106663f7bd99
sajal <Jainsajal519@gmail.com>
2025-05-05 00:58:34 +0530

    Initial Setup for routing

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,41 @@
 import React, { useState } from 'react'
 import Cards from './Components/Cards'
 import Form from './Components/Form'
+import { Link, Route, Routes } from 'react-router-dom'
+import Home from './Components/Home'
 
-function App() {
-  const [users,setUser]=useState([]);
-  const handleUser = (data) => {
-    setUser([...users, data]);
-  }
-      const handleRemove=(id)=>{
-        setUser(users.filter((user,index)=>index!==id))
-    }
+function App(){
   return (
-    <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
-      <div className='container mx-auto'>
-      <Cards handleRemove={handleRemove} users = {users}/>
-      <Form handleUser={handleUser}/>
-      </div>
-    </div>
+            <>
+            <nav>
+            <Link to='/home' >App</Link>
+            </nav>
+            <Routes>
+              <Route path="/home" element={<Home/>} />
+            </Routes>
+            </>
   )
 }
-
 export default App
+
+
+
+//---------Form with card----------------
+
+// function App() {
+//   const [users,setUser]=useState([]);
+//   const handleUser = (data) => {
+//     setUser([...users, data]);
+//   }
+//       const handleRemove=(id)=>{
+//         setUser(users.filter((user,index)=>index!==id))
+//     }
+//   return (
+//     <div className='w-full h-screen bg-zinc-200 flex items-center justify-center'>
+//       <div className='container mx-auto'>
+//       <Cards handleRemove={handleRemove} users = {users}/>
+//       <Form handleUser={handleUser}/>
+//       </div>
+//     </div>
+//   )
+// }
